246b82e4e364f5729e6ab3f71605998f8c2a2534
chechetkin <32839921+BeastChe@users.noreply.github.com>
2017-10-19 20:15:36 +0300

    матчи за 7 дней
    начал делать bets

--- bets.py
+++ bets.py
@@ -1,14 +1,13 @@
 import requests
 import json
+import matches
 import rndUserInfo
 import userInfo
 
+if matches
 
 url = 'https://api.trbna.com/betting_insider/ru/mobile/v2/bets/'
-payload =
-{
-  "data": {
-    "odds_id": "1488",
+payload = {"data": {"odds_id": "1488",
     "amount": 500,
     "rate": 2.15,
     "comment": "А вот так вот )"
--- matches.py
+++ matches.py
@@ -3,8 +3,6 @@ import datetime
 
 
 
-
-
 today = datetime.date.today()
 DAY = []
 for i in range (0, 7):
@@ -14,20 +12,45 @@ for i in range (0, 7):
     payload = {'date': DAY[i], 'category_id': '208', 'bookmaker_id': 'default', 'line_type': 'default', 'tournament_id': 'default', 'include': 'team,odds'}
     headers = {'content-type': 'application/json'}
     matches = requests.get(url, params=payload,  headers=headers)
-    print(matches.url)
-    print(matches.text)
+    SIZES = []
+    def give_size():
+        print(i)
+        str = matches.text
+        print(str)
+        size = str.count("rate")
+        print(size)
+        SIZES.append(size)
+        print(SIZES)
+        return SIZES
+        RATES = []
+        def give_rates():
+            for x in range(0, SIZES[i]):
+                response = matches.json()
+                rate = response['include']['odds'][x]['rate']
+                RATES.append(rate)
+            return RATES
+        ODDS_ID = []
+        def give_odds():
+            for x in range(0, SIZES[i]):
+                response = matches.json()
+                odds_id = response['include']['odds'][x]['id']
+                ODDS_ID.append(odds_id)
+            return ODDS_ID
 
 
-str = matches.text
-size = str.count("rate")
+A = []
+B = []
+C = []
+
+A = give_size()
+#B = give_rates()
+#C = give_odds()
+
+print(A)
+#print(B)
+#print(C)
 
 
-def give_rate():
-    RATES = []
-    for i in range(0, size):
-        response = matches.json()
-        rate = response['include']['odds'][i]['rate']
-        RATES.append(rate)
 
 
 
